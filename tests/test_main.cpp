5c10c0ef2acddd11157563802ed4e3ea9b5d9f5e
Emir <emir067@inbox.ru~>
2025-01-01 17:13:33 +0300

    Внедрение Redis в юнит тесты

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,19 +1,8 @@
 #define CATCH_CONFIG_MAIN  // Эта строка указывает Catch2 создать функцию main
 #include <catch2/catch.hpp>
-#include "include/Database.h"
+#include "Database.h"
 
-TEST_CASE("Database connection test", "[database]") {
-    // Проверка подключения к базе данных
-    Database db("postgresql://user:password@localhost/dbname");
-    REQUIRE(db.isConnected() == true);
-}
-
-TEST_CASE("Database query test", "[database]") {
-    Database db("postgresql://user:password@localhost/dbname");
-    REQUIRE(db.isConnected() == true);
-
-    // Проверка выполнения SQL-запроса
-    auto result = db.execute("SELECT 1");
-    REQUIRE(result.size() == 1);
-    REQUIRE(result[0][0].as<int>() == 1);
+TEST_CASE("Database connection") {
+    Database db("postgresql://user:password@localhost/dbname", "127.0.0.1", 6379);
+    REQUIRE_NOTHROW(db.createTest("Sample Test"));
 }
