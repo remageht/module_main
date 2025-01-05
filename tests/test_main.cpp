6a3e7d97543de724bcd0742459a4c03b3d64c5b0
Emir <emir067@inbox.ru~>
2025-01-05 13:58:29 +0300

    была добавлена библиотека openssl

--- tests/test_main.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_CONFIG_MAIN  // Эта строка указывает Catch2 создать функцию main
-#include <catch2/catch.hpp>
-#include "Database.h"
-
-TEST_CASE("Database connection") {
-    Database db("postgresql://user:password@localhost/dbname", "127.0.0.1", 6379);
-    REQUIRE_NOTHROW(db.createTest("Sample Test"));
-}
